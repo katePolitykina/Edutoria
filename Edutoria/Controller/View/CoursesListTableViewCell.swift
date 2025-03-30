f9092644f7c0d8c28b9d9d4d05b1a6f0ab2f6dd3
ekaterinapolitykina <eka.politykina@gmail.com>
2025-03-30 22:07:24 +0300

    Lessons list

---
+++ Edutoria/Controller/View/CoursesListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CoursesListTableViewCell.swift
+//  Edutoria
+//
+//  Created by Ekaterina Politykina on 28.03.25.
+//
+
+import UIKit
+
+class CoursesListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
